88fd1e180d1ae3fb24244eb7193792ccea5b2e2f
Guillaume Lessard <dhtnstff@gmail.com>
2015-03-06 15:39:51 -0700

    Simple, more swiftian wrappers for libdispatch.

---
+++ ExampleResultChain/main.swift
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  ExampleResultChain
+//
+//  Created by Guillaume Lessard on 2015-03-06.
+//  Copyright (c) 2015 Guillaume Lessard. All rights reserved.
+//
+
+import Darwin
+
+syncprint("Starting")
+
+let result1 = async {
+  _ -> Double in
+  syncprint("Starting result1")
+  sleep(1)
+  syncprint("Finishing result1")
+  return 10.1
+}
+
+let result2 = result1.notify {
+  (d: Double) -> Int in
+  syncprint("Starting result2")
+  sleep(1)
+  syncprint("Finishing result2")
+  return Int(floor(2*d))
+}
+
+let result3 = result1.notify { return (3*$0).description }
+
+result3.notify { syncprint($0) }
+
+syncprint("Waiting")
+syncprint(result1.get())
+syncprint(result2.get())
+syncprint(result3.get())
+syncprint("Done")
+syncprintwait()
---
+++ utilities/syncprint.swift
@@ -0,0 +1,53 @@
+//
+//  syncprint.swift
+//
+//  Created by Guillaume Lessard on 2014-08-22.
+//  Copyright (c) 2014 Guillaume Lessard. All rights reserved.
+//
+//  https://gist.github.com/glessard/826241431dcea3655d1e
+//
+
+import Dispatch
+import Foundation.NSThread
+
+private var PrintQueue: dispatch_queue_attr_t! =
+                        dispatch_queue_create("com.tffenterprises.syncprint", DISPATCH_QUEUE_SERIAL)
+
+private var PrintGroup: dispatch_group_t! = dispatch_group_create()
+
+/**
+  A wrapper for println that runs all requests on a serial queue
+
+  Writes a basic thread identifier (main or back), the textual representation
+  of `object`, and a newline character onto the standard output.
+
+  The textual representation is obtained from the `object` using its protocol
+  conformances, in the following order of preference: `Streamable`,
+  `Printable`, `DebugPrintable`.
+
+  :param: object the item to be printed
+*/
+
+public func syncprint<T>(object: T)
+{
+  var message = NSThread.currentThread().isMainThread ? "[main] " : "[back] "
+
+  assert(PrintQueue != nil && PrintGroup != nil, "Init failure in logging.swift")
+  dispatch_group_async(PrintGroup, PrintQueue) { print(object, &message); println(message) }
+}
+
+/**
+  Block until all tasks created by syncprint() have completed.
+*/
+
+public func syncprintwait()
+{
+  // Wait at most 200ms for the last messages to print out.
+  let res = dispatch_group_wait(PrintGroup, dispatch_time(DISPATCH_TIME_NOW, 200_000_000))
+  if res != 0
+  {
+    dispatch_suspend(PrintQueue)
+    NSThread.sleepForTimeInterval(0.1)
+    println("Giving up on waiting")
+  }
+}
