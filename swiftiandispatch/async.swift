d0e8dc0afe66f40a1cfdf59266b032af56be4100
Guillaume Lessard <dhtnstff@gmail.com>
2015-07-09 18:13:36 -0600

    Renamed `Result` to `Deferred`
    It makes sense and doesn't clash with the typical use of a `Result` type.
    `Deferred` also happens to be the name of a module of similar intent in ocaml.
    
    Note that apart from the blocking `value` call, `Deferred` is lock-free.

--- swiftiandispatch/async.swift
+++ swiftiandispatch/async.swift
@@ -56,181 +56,34 @@ public func async(queue: dispatch_queue_t, group: dispatch_group_t, task: () -> 
   dispatch_group_async(group, queue, task)
 }
 
-/**
-  An asynchronous computation result.
-
-  The get() method will return the result, blocking until it is ready.
-  If the result is ready when get() is called, it will return immediately.
-*/
-
-public struct Result<T>
-{
-  internal let group: dispatch_group_t
-  internal let result: () -> T
-
-  public var value: T { return result() }
-}
-
 // MARK: Asynchronous tasks with return values.
 
-public func async<T>(task: () -> T) -> Result<T>
+public func async<T>(task: () -> T) -> Deferred<T>
 {
   return async(dispatch_get_global_queue(qos_class_self(), 0), task: task)
 }
 
-public func async<T>(group group: dispatch_group_t, task: () -> T) -> Result<T>
+public func async<T>(group group: dispatch_group_t, task: () -> T) -> Deferred<T>
 {
   return async(dispatch_get_global_queue(qos_class_self(), 0), group: group, task: task)
 }
 
-public func async<T>(qos: qos_class_t, task: () -> T) -> Result<T>
+public func async<T>(qos: qos_class_t, task: () -> T) -> Deferred<T>
 {
   return async(dispatch_get_global_queue(qos, 0), task: task)
 }
 
-public func async<T>(qos: qos_class_t, group: dispatch_group_t, task: () -> T) -> Result<T>
+public func async<T>(qos: qos_class_t, group: dispatch_group_t, task: () -> T) -> Deferred<T>
 {
   return async(dispatch_get_global_queue(qos, 0), group: group, task: task)
 }
 
-public func async<T>(queue: dispatch_queue_t, task: () -> T) -> Result<T>
+public func async<T>(queue: dispatch_queue_t, task: () -> T) -> Deferred<T>
 {
-  let g = dispatch_group_create()!
-  var result: T! = nil
-
-  dispatch_group_enter(g)
-  dispatch_async(queue) {
-    result = task()
-    dispatch_group_leave(g)
-  }
-
-  return Result(group: g) {
-    () -> T in
-    dispatch_group_wait(g, DISPATCH_TIME_FOREVER)
-    return result
-  }
+  return Deferred(queue: queue, task: task)
 }
 
-public func async<T>(queue: dispatch_queue_t, group: dispatch_group_t, task: () -> T) -> Result<T>
+public func async<T>(queue: dispatch_queue_t, group: dispatch_group_t, task: () -> T) -> Deferred<T>
 {
-  let g = dispatch_group_create()!
-  var result: T! = nil
-
-  dispatch_group_enter(g)
-  dispatch_group_enter(group)
-  dispatch_async(queue) {
-    result = task()
-    dispatch_group_leave(group)
-    dispatch_group_leave(g)
-  }
-
-  return Result(group: g) {
-    () -> T in
-    dispatch_group_wait(g, DISPATCH_TIME_FOREVER)
-    return result
-  }
-}
-
-// MARK: Asynchronous tasks with input parameters and no return values.
-
-extension Result
-{
-  public func notify(task: (T) -> ())
-  {
-    return notify(dispatch_get_global_queue(qos_class_self(), 0), task: task)
-  }
-
-  public func notify(group group: dispatch_group_t, task: (T) -> ())
-  {
-    return notify(dispatch_get_global_queue(qos_class_self(), 0), group: group, task: task)
-  }
-
-  public func notify(qos: qos_class_t, task: (T) -> ())
-  {
-    return notify(dispatch_get_global_queue(qos, 0), task: task)
-  }
-
-  public func notify(qos: qos_class_t, group: dispatch_group_t, task: (T) -> ())
-  {
-    return notify(dispatch_get_global_queue(qos, 0), group: group, task: task)
-  }
-
-  public func notify(queue: dispatch_queue_t, task: (T) -> ())
-  {
-    dispatch_group_notify(self.group, queue) {
-      task(self.result())
-    }
-  }
-
-  public func notify(queue: dispatch_queue_t, group: dispatch_group_t, task: (T) -> ())
-  {
-    dispatch_group_enter(group)
-    dispatch_group_notify(self.group, queue) {
-      task(self.result())
-      dispatch_group_leave(group)
-    }
-  }
-}
-
-// MARK: Asynchronous tasks with input parameters and return values
-
-extension Result
-{
-  public func notify<U>(task: (T) -> U) -> Result<U>
-  {
-    return notify(dispatch_get_global_queue(qos_class_self(), 0), task: task)
-  }
-
-  public func notify<U>(group group: dispatch_group_t, task: (T) -> U) -> Result<U>
-  {
-    return notify(dispatch_get_global_queue(qos_class_self(), 0), group: group, task: task)
-  }
-  
-  public func notify<U>(qos: qos_class_t, task: (T) -> U) -> Result<U>
-  {
-    return notify(dispatch_get_global_queue(qos, 0), task: task)
-  }
-
-  public func notify<U>(qos: qos_class_t, group: dispatch_group_t, task: (T) -> U) -> Result<U>
-  {
-    return notify(dispatch_get_global_queue(qos, 0), group: group, task: task)
-  }
-  
-  public func notify<U>(queue: dispatch_queue_t, task: (T) -> U) -> Result<U>
-  {
-    let g = dispatch_group_create()!
-    var result: U! = nil
-
-    dispatch_group_enter(g)
-    dispatch_group_notify(self.group, queue) {
-      result = task(self.result())
-      dispatch_group_leave(g)
-    }
-
-    return Result<U>(group: g) {
-      () -> U in
-      dispatch_group_wait(g, DISPATCH_TIME_FOREVER)
-      return result
-    }
-  }
-
-  public func notify<U>(queue: dispatch_queue_t, group: dispatch_group_t, task: (T) -> U) -> Result<U>
-  {
-    let g = dispatch_group_create()!
-    var result: U! = nil
-
-    dispatch_group_enter(g)
-    dispatch_group_enter(group)
-    dispatch_group_notify(self.group, queue) {
-      result = task(self.result())
-      dispatch_group_leave(group)
-      dispatch_group_leave(g)
-    }
-
-    return Result<U>(group: g) {
-      () -> U in
-      dispatch_group_wait(g, DISPATCH_TIME_FOREVER)
-      return result
-    }
-  }
+  return Deferred(queue: queue, group: group, task: task)
 }
---
+++ swiftiandispatch/deferred.swift
@@ -0,0 +1,180 @@
+//
+//  deferred.swift
+//  swiftiandispatch
+//
+//  Created by Guillaume Lessard on 2015-07-09.
+//  Copyright © 2015 Guillaume Lessard. All rights reserved.
+//
+
+import Dispatch
+
+/**
+  An asynchronous computation result.
+
+  The get() method will return the result, blocking until it is ready.
+  If the result is ready when get() is called, it will return immediately.
+*/
+
+public class Deferred<T>
+{
+  private var v: T! = nil
+  private let group = dispatch_group_create()
+
+  private var currentState: Int32 = State.Ready.rawValue
+
+  private func setState(newState: State) -> Bool
+  {
+    switch newState
+    {
+    case .Ready:
+      return currentState == State.Ready.rawValue
+
+    case .Running:
+      return OSAtomicCompareAndSwap32Barrier(State.Ready.rawValue, State.Running.rawValue, &currentState)
+
+//    case .Canceled:
+//      let s = currentState
+//      if s == State.Completed.rawValue { return false }
+//      return OSAtomicCompareAndSwap32Barrier(s, State.Canceled.rawValue, &currentState)
+
+    case .Assigning:
+      return OSAtomicCompareAndSwap32Barrier(State.Running.rawValue, State.Assigning.rawValue, &currentState)
+
+    case .Completed:
+      if OSAtomicCompareAndSwap32Barrier(State.Assigning.rawValue, State.Completed.rawValue, &currentState)
+      {
+        dispatch_group_leave(group)
+        return true
+      }
+      return false
+    }
+  }
+
+  private init()
+  {
+    dispatch_group_enter(group)
+  }
+
+  public convenience init(queue: dispatch_queue_t, task: () -> T)
+  {
+    self.init()
+
+    guard setState(.Running) else { fatalError("Could not start task in \(__FUNCTION__)") }
+    dispatch_async(queue) {
+      self.setValue(task())
+    }
+  }
+
+  public convenience init(queue: dispatch_queue_t, group: dispatch_group_t, task: () -> T)
+  {
+    dispatch_group_enter(group)
+    self.init(queue: queue) {
+      let result = task()
+      dispatch_group_leave(group)
+      return result
+    }
+  }
+
+  private func setValue(result: T)
+  {
+    guard setState(.Assigning) else { fatalError("Could not begin assignment of result in \(__FUNCTION__)") }
+
+    v = result
+
+    guard setState(.Completed) else { fatalError("Could not complete assignment of result in \(__FUNCTION__)") }
+  }
+
+  public var value: T {
+    if currentState != State.Completed.rawValue
+    {
+      dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
+    }
+    return v
+  }
+}
+
+private enum State: Int32 { case Ready = 0, Running = 1, /* Canceled = 2, */ Completed = 3, Assigning = 99 }
+
+
+
+// MARK: Asynchronous tasks with input parameters and no return values.
+
+extension Deferred
+{
+  public func notify(task: (T) -> ())
+  {
+    return notify(dispatch_get_global_queue(qos_class_self(), 0), task: task)
+  }
+
+  public func notify(group group: dispatch_group_t, task: (T) -> ())
+  {
+    return notify(dispatch_get_global_queue(qos_class_self(), 0), group: group, task: task)
+  }
+
+  public func notify(qos: qos_class_t, task: (T) -> ())
+  {
+    return notify(dispatch_get_global_queue(qos, 0), task: task)
+  }
+
+  public func notify(qos: qos_class_t, group: dispatch_group_t, task: (T) -> ())
+  {
+    return notify(dispatch_get_global_queue(qos, 0), group: group, task: task)
+  }
+
+  public func notify(queue: dispatch_queue_t, task: (T) -> ())
+  {
+    dispatch_group_notify(self.group, queue) {
+      task(self.value)
+    }
+  }
+
+  public func notify(queue: dispatch_queue_t, group: dispatch_group_t, task: (T) -> ())
+  {
+    dispatch_group_enter(group)
+    dispatch_group_notify(self.group, queue) {
+      task(self.value)
+      dispatch_group_leave(group)
+    }
+  }
+}
+
+// MARK: Asynchronous tasks with input parameters and return values
+
+extension Deferred
+{
+  public func notify<U>(task: (T) -> U) -> Deferred<U>
+  {
+    return notify(dispatch_get_global_queue(qos_class_self(), 0), task: task)
+  }
+
+  public func notify<U>(group group: dispatch_group_t, task: (T) -> U) -> Deferred<U>
+  {
+    return notify(dispatch_get_global_queue(qos_class_self(), 0), group: group, task: task)
+  }
+
+  public func notify<U>(qos: qos_class_t, task: (T) -> U) -> Deferred<U>
+  {
+    return notify(dispatch_get_global_queue(qos, 0), task: task)
+  }
+
+  public func notify<U>(qos: qos_class_t, group: dispatch_group_t, task: (T) -> U) -> Deferred<U>
+  {
+    return notify(dispatch_get_global_queue(qos, 0), group: group, task: task)
+  }
+
+  public func notify<U>(queue: dispatch_queue_t, task: (T) -> U) -> Deferred<U>
+  {
+    return Deferred<U>(queue: queue) { task(self.value) }
+  }
+
+  public func notify<U>(queue: dispatch_queue_t, group: dispatch_group_t, task: (T) -> U) -> Deferred<U>
+  {
+    dispatch_group_enter(group)
+
+    return Deferred<U>(queue: queue, group: group) {
+      let result = task(self.value)
+      dispatch_group_leave(group)
+      return result
+    }
+  }
+}
