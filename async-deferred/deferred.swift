fc1f2f53c063fb611aa2f3536db188f76c073fed
Guillaume Lessard <dhtnstff@gmail.com>
2015-08-17 10:39:25 -0600

    Layout tweak

--- async-deferred/deferred.swift
+++ async-deferred/deferred.swift
@@ -58,7 +58,10 @@ public class Deferred<T>
 
   deinit
   {
-    if v == nil { dispatch_group_leave(group) }
+    if v == nil
+    {
+      dispatch_group_leave(group)
+    }
   }
 
   /// Initialize to an already determined state
