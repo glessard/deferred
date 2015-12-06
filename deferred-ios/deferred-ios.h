7cf959966065d834a87f7be16b54f5d6eb302a51
Guillaume Lessard <dhtnstff@gmail.com>
2015-12-05 23:26:19 -0700

    Updated project file and other support files.

---
+++ deferred-ios/deferred-ios.h
@@ -0,0 +1,19 @@
+//
+//  deferred-ios.h
+//  deferred-ios
+//
+//  Created by Guillaume Lessard on 05/12/2015.
+//  Copyright © 2015 Guillaume Lessard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for deferred-ios.
+FOUNDATION_EXPORT double deferred_iosVersionNumber;
+
+//! Project version string for deferred-ios.
+FOUNDATION_EXPORT const unsigned char deferred_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <deferred_ios/PublicHeader.h>
+
+
---
+++ deferred/deferred.h
@@ -0,0 +1,19 @@
+//
+//  deferred.h
+//  deferred
+//
+//  Created by Guillaume Lessard on 05/12/2015.
+//  Copyright © 2015 Guillaume Lessard. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for deferred.
+FOUNDATION_EXPORT double deferredVersionNumber;
+
+//! Project version string for deferred.
+FOUNDATION_EXPORT const unsigned char deferredVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <deferred/PublicHeader.h>
+
+
