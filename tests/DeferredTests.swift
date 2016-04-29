20d4d5559516edc916d3e9778b43a6f87c833d56
Guillaume Lessard <dhtnstff@gmail.com>
2016-04-29 15:15:05 -0600

    tweak timing tests

--- tests/DeferredTests.swift
+++ tests/DeferredTests.swift
@@ -772,9 +772,10 @@ class DeferredTests: XCTestCase
   func testPropagationTime()
   {
     let iterations = 10_000
-
     let ref = NSDate.distantPast()
-    var dt = Deferred(value: (0, ref, ref))
+
+    let first = TBD<(Int, NSDate, NSDate)>(qos: QOS_CLASS_USER_INITIATED)
+    var dt: Deferred = first
     for _ in 0...iterations
     {
       dt = dt.map {
@@ -783,28 +784,36 @@ class DeferredTests: XCTestCase
       }
     }
 
-    if let (iterations, tic, toc) = dt.value
+    try! first.determine( (0, ref, ref) )
+
+    switch dt.result
     {
-      print("\(round(Double(toc.timeIntervalSinceDate(tic)*1e9)/Double(iterations))/1000) µs per message")
+    case let .value(iterations, tic, toc):
+      let interval = toc.timeIntervalSinceDate(tic)
+      print("\(round(Double(interval*1e9)/Double(iterations))/1000) µs per message")
+
+    default: XCTFail()
     }
   }
 
   func testNotificationTime()
   {
-    let iterations = 100_000
+    let iterations = 10_000
 
     let attr = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_USER_INITIATED, 0)
-    let dt = Deferred(value: 1).notifying(on: dispatch_queue_create("", attr))
-    let start = NSDate()
+    let start = TBD<NSDate>(queue: dispatch_queue_create("", attr))
     for _ in 0..<iterations
     {
-      dt.notify { _ in }
+      start.notify { _ in }
     }
 
-    switch dt.map(transform: { _ in NSDate() }).result
+    let dt = start.map { start in NSDate().timeIntervalSinceDate(start) }
+    try! start.determine(NSDate())
+
+    switch dt.result
     {
-    case .value(let end):
-      print("\(round(Double(end.timeIntervalSinceDate(start)*1e9)/Double(iterations))/1000) µs per notification")
+    case .value(let interval):
+      print("\(round(Double(interval*1e9)/Double(iterations))/1000) µs per notification")
 
     default: XCTFail()
     }
