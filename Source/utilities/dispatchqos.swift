77ea8259fc1c58c08eade0bfa5fa5664d8502266
Guillaume Lessard <dhtnstff@gmail.com>
2016-11-09 03:08:06 -0700

    an attempt to compile on linux

--- Source/utilities/dispatchqos.swift
+++ Source/utilities/dispatchqos.swift
@@ -11,11 +11,18 @@ import Dispatch
 extension DispatchQoS
 {
 #if SWIFT_PACKAGE
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
   public static func current(fallback: DispatchQoS.QoSClass = .utility) -> DispatchQoS
   {
     let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? fallback
     return DispatchQoS(qosClass: qos, relativePriority: 0)
   }
+#else // presumably Linux or Windows
+  public static func current(fallback: DispatchQoS.QoSClass = .utility) -> DispatchQoS
+  {
+    return DispatchQoS(qosClass: fallback, relativePrority: 0)
+  }
+#endif
 #else
   static func current(fallback: DispatchQoS.QoSClass = .utility) -> DispatchQoS
   {
