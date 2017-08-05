ded144e6312b9b32cc0f5476c8b8449e91beecec
Guillaume Lessard <dhtnstff@gmail.com>
2017-08-05 01:10:34 -0600

    update package description to swift 4 version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,26 @@
+// swift-tools-version:4.0
+
 import PackageDescription
+
+#if swift(>=4.0)
+
+let package = Package(
+  name: "deferred",
+  products: [
+    .library(name: "deferred", type: .static, targets: ["deferred"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/glessard/syncprint.git", from: "2.0.0"),
+    .package(url: "https://github.com/glessard/swift-atomics.git", from: "3.0.0"),
+  ],
+  targets: [
+    .target(name: "deferred", dependencies: [ .product(name: "Atomics") ], path: "Source"),
+    .testTarget(name: "deferredTests", dependencies: [ "deferred", .product(name: "syncprint") ]),
+  ],
+  swiftLanguageVersions: [4]
+)
+
+#else
 
 let package = Package(
   name: "deferred",
@@ -10,3 +32,5 @@ let package = Package(
     .Package(url: "https://github.com/glessard/swift-atomics.git", majorVersion: 3),
   ]
 )
+
+#endif
