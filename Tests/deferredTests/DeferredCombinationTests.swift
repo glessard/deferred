80aa9c3e562a2f54ec4eabeb99f5de32ca7f2e87
Guillaume Lessard <dhtnstff@gmail.com>
2018-07-18 11:57:19 -0600

    silence a Swift 3.2 warning

--- Tests/deferredTests/DeferredCombinationTests.swift
+++ Tests/deferredTests/DeferredCombinationTests.swift
@@ -275,7 +275,7 @@ class DeferredCombinationTimedTests: XCTestCase
 {
   let loopTestCount = 5_000
 
-#if swift(>=4.0) && (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) || (swift(>=4.1) && os(Linux))
+#if (swift(>=4.0) && (os(macOS) || os(iOS) || os(tvOS) || os(watchOS))) || (swift(>=4.1) && os(Linux))
   var metrics: [XCTPerformanceMetric] { return DeferredCombinationTimedTests.defaultPerformanceMetrics }
 #else
   var metrics: [String] { return DeferredCombinationTimedTests.defaultPerformanceMetrics() }
