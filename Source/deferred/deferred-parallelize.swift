772971ef235572d405c2ac1f9c047a8be2c82c80
Guillaume Lessard <dhtnstff@gmail.com>
2018-08-31 11:09:23 -0600

    remove last accommodations for Swift 3.1.x

--- Source/deferred/deferred-parallelize.swift
+++ Source/deferred/deferred-parallelize.swift
@@ -40,54 +40,6 @@ extension Deferred
   }
 }
 
-#if swift(>=3.2)
-extension Collection
-{
-  /// Map a collection to an array of `Deferred` to be computed in parallel, at the desired QoS level
-  ///
-  /// - parameter qos: the QoS at which the parallel task should be performed; defaults to the current QoS class.
-  /// - parameter task: the computation to be performed in parallel
-  /// - returns: an array of `Deferred`
-  /// - parameter element: an element to transform into a new `Deferred`
-
-  public func deferredMap<Value>(qos: DispatchQoS = .current,
-                                 task: @escaping (_ element: Self.Iterator.Element) throws -> Value) -> [Deferred<Value>]
-  {
-    let queue = DispatchQueue(label: "deferred-map", qos: qos)
-    return deferredMap(queue: queue, task: task)
-  }
-
-  /// Map a collection to an array of `Deferred` to be computed in parallel, on the desired dispatch queue
-  ///
-  /// - parameter queue: the `DispatchQueue` on which the parallel task should be performed.
-  /// - parameter task: the computation to be performed in parallel
-  /// - returns: an array of `Deferred`
-  /// - parameter element: an element to transform into a new `Deferred`
-
-  public func deferredMap<Value>(queue: DispatchQueue,
-                                 task: @escaping (_ element: Self.Iterator.Element) throws -> Value) -> [Deferred<Value>]
-  {
-    let count: Int = numericCast(self.count)
-    let deferreds = (0..<count).map { _ in TBD<Value>(queue: queue) }
-    let indexList = Array(self.indices)
-
-    queue.async {
-      DispatchQueue.concurrentPerform(iterations: count) {
-        iteration in
-        deferreds[iteration].beginExecution()
-        do {
-          let value = try task(self[indexList[iteration]])
-          deferreds[iteration].determine(value)
-        }
-        catch {
-          deferreds[iteration].determine(error)
-        }
-      }
-    }
-    return deferreds
-  }
-}
-#else
 extension Collection where Index == Indices.Iterator.Element
 {
   /// Map a collection to an array of `Deferred` to be computed in parallel, at the desired QoS level
@@ -134,4 +86,3 @@ extension Collection where Index == Indices.Iterator.Element
     return deferreds
   }
 }
-#endif
