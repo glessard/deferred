07d1b36f8415a51f56ea56363d97de15217c1f81
Guillaume Lessard <dhtnstff@gmail.com>
2019-03-26 22:53:59 -0600

    a different version of Atomics (swift-atomics 5.0.0-b4)
    - and update the other submodules as well

--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -8,9 +8,9 @@ let package = Package(
     .library(name: "deferred", type: .static, targets: ["deferred"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/glessard/swift-atomics.git", from: "4.4.1"),
-    .package(url: "https://github.com/glessard/outcome.git", from: "4.1.5"),
-    .package(url: "https://github.com/glessard/CurrentQoS.git", from: "1.0.0"),
+    .package(url: "https://github.com/glessard/swift-atomics.git", .revision("4f628bdf26a1efba2fa3673b22c21560bd9fec03")),
+    .package(url: "https://github.com/glessard/outcome.git", from: "4.2.0"),
+    .package(url: "https://github.com/glessard/CurrentQoS.git", from: "1.1.0"),
   ],
   targets: [
     .target(name: "deferred", dependencies: ["CAtomics", "Outcome", "CurrentQoS"]),
